69e3ebe4f8555f857d5d5acbcb6044c1d57a797b
it13manoj <it13manoj@gmail.com>
2026-09-12 17:55:37 +0530

    update

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyparser.json({ limit: '50mb' }));
 app.use(
     "/uploads",
     express.static(path.join(__dirname, "Src/uploads"))
